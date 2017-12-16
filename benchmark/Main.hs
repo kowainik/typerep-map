f517f30e91b49aa4064df4c0e8a4a56053b60ba6
vrom911 <vrom911@gmail.com>
2017-12-16 18:57:11 +0300

    Add benchmarks for TypeRepMap

--- benchmark/Main.hs
+++ benchmark/Main.hs
@@ -1,4 +1,31 @@
-import Criterion.Main
+{-# LANGUAGE ExplicitForAll      #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
+import Criterion.Main (bench, defaultMain, whnf)
+
+import Prelude hiding (lookup)
+
+import Data.Proxy (Proxy (..))
+import Data.Typeable (Typeable)
+
+import Data.TypeRep.Map (TypeRepMap, empty, insert, lookup, size)
 
 main :: IO ()
-main = defaultMain [bench "const" (whnf const ())]
+main = do
+    putStrLn $ "size: " ++ (show $ size bigMap)
+    defaultMain
+        [ bench "lookup"     $ whnf (lookup :: TypeRepMap -> Maybe Char) bigMap
+        , bench "insert new" $ whnf (\x -> insert x bigMap) (111 :: Int)
+        , bench "update old" $ whnf (\x -> insert x bigMap) 'b'
+        ]
+
+-- TypeRepMap of 10000 elements
+bigMap :: TypeRepMap
+bigMap = buildBigMap 10000 (Proxy :: Proxy Z) empty
+
+data Z
+data S a
+
+buildBigMap :: forall a . Typeable a => Int -> Proxy a -> TypeRepMap -> TypeRepMap
+buildBigMap 1 x = insert 'a' . insert x
+buildBigMap n x = insert x . buildBigMap (n - 1) (Proxy :: Proxy (S a))
