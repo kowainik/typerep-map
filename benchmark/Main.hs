5de021d6db0671e263ed6cbe0d84aba527927d49
Vladislav Zavialov <vlad.z.4096@gmail.com>
2017-12-17 17:41:01 +0300

    Use type-level Nat

--- benchmark/Main.hs
+++ benchmark/Main.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE DataKinds            #-}
+{-# LANGUAGE PolyKinds            #-}
 {-# LANGUAGE ExplicitForAll       #-}
 {-# LANGUAGE ExplicitNamespaces   #-}
 {-# LANGUAGE KindSignatures       #-}
@@ -6,6 +7,8 @@
 {-# LANGUAGE TypeFamilies         #-}
 {-# LANGUAGE TypeOperators        #-}
 {-# LANGUAGE UndecidableInstances #-}
+
+{-# OPTIONS_GHC -fplugin GHC.TypeLits.KnownNat.Solver #-}
 
 import Criterion.Main (bench, defaultMain, nf, whnf)
 
@@ -16,7 +19,7 @@ import Control.Exception.Base (evaluate)
 import Data.Maybe (fromJust)
 import Data.Proxy (Proxy (..))
 import Data.Typeable (Typeable)
-import GHC.TypeLits (type (-), Nat)
+import GHC.TypeLits
 
 import Data.TypeRep.Map (TypeRepMap (..), empty, insert, keys, lookup, size)
 
@@ -26,31 +29,29 @@ main = do
     evaluate $ rknf bigMap
     defaultMain
         [ bench "lookup"     $ nf tenLookups bigMap
-        , bench "insert new" $ whnf (\x -> rknf $ insert x bigMap) (proxy (111 :: Int))
-        , bench "update old" $ whnf (\x -> rknf $ insert x bigMap) (proxy 'b')
+        , bench "insert new" $ whnf (\x -> rknf $ insert x bigMap) (Proxy :: Proxy 9999999999)
+        , bench "update old" $ whnf (\x -> rknf $ insert x bigMap) (Proxy :: Proxy 1)
         ]
 
-tenLookups :: TypeRepMap Proxy
-           -> ( Proxy (BigProxy 10), Proxy (BigProxy 20)
-              , Proxy (BigProxy 30), Proxy (BigProxy 40)
-              , Proxy (BigProxy 50), Proxy (BigProxy 60)
-              , Proxy (BigProxy 70), Proxy (BigProxy 80)
+tenLookups :: TypeRepMap (Proxy :: Nat -> *)
+           -> ( Proxy 10, Proxy 20, Proxy 30, Proxy 40
+              , Proxy 50, Proxy 60, Proxy 70, Proxy 80
               )
 tenLookups tmap = (lp, lp, lp, lp, lp, lp, lp, lp)
   where
-    lp :: Typeable a => Proxy a
+    lp :: forall (a::Nat). Typeable a => Proxy a
     lp = fromJust $ lookup tmap
 
 -- TypeRepMap of 10000 elements
-bigMap :: TypeRepMap Proxy
-bigMap = buildBigMap 10000 (Proxy :: Proxy Z) empty
+bigMap :: TypeRepMap (Proxy :: Nat -> *)
+bigMap = buildBigMap 10000 (Proxy :: Proxy 0) empty
 
 data Z
 data S a
 
-buildBigMap :: forall a . Typeable a => Int -> Proxy a -> TypeRepMap Proxy -> TypeRepMap Proxy
-buildBigMap 1 x = insert (proxy 'a') . insert x
-buildBigMap n x = insert x . buildBigMap (n - 1) (Proxy :: Proxy (S a))
+buildBigMap :: forall a . (Typeable a, KnownNat a) => Int -> Proxy (a :: Nat) -> TypeRepMap (Proxy :: Nat -> *) -> TypeRepMap (Proxy :: Nat -> *)
+buildBigMap 1 x = insert x
+buildBigMap n x = insert x . buildBigMap (n - 1) (Proxy :: Proxy (a + 1))
 
 rknf :: TypeRepMap f -> ()
 rknf = rnf . keys
@@ -58,6 +59,3 @@ rknf = rnf . keys
 type family BigProxy (n :: Nat) :: * where
     BigProxy 0 = Z
     BigProxy n = S (BigProxy (n - 1))
-
-proxy :: a -> Proxy a
-proxy _ = Proxy
