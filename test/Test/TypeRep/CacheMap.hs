50606e94866f7a88571de3ebef28e06630e5ca8e
Dmitry Kovanikov <kovanikov@gmail.com>
2018-07-07 17:34:05 +0800

    [#7] Implement insert for CachedMap

--- test/Test/TypeRep/CacheMap.hs
+++ test/Test/TypeRep/CacheMap.hs
@@ -3,10 +3,9 @@ module Test.TypeRep.CacheMap where
 import Prelude hiding (lookup)
 
 import Data.Functor.Identity (Identity (..))
+import Test.Tasty.Hspec (Spec, describe, it, shouldBe)
 
-import Test.Tasty.Hspec
-
-import Data.TypeRep.CacheMap (TF (..), fromList, lookup)
+import Data.TypeRep.CacheMap (TF (..), TypeRepMap, empty, fromList, insert, lookup, size)
 
 -- Simple test for 'lookup', 'insert' and 'size' functions.
 spec_insertLookup :: Spec
@@ -17,25 +16,25 @@ spec_insertLookup = do
        -- it "returns the second inserted value of the same type" $
        --     lookup (fromList [TF (Identity 'b'), TF (Identity 'a')]) `shouldBe` Just (Identity 'b')
 
---    describe "Size Test" $ do
---        it "is empty" $
---            size empty `shouldBe` 0
---        it "is of size 1 when 1 element inserted" $
---            size (insert (Identity 'a') empty) `shouldBe` 1
---        it "doesn't increase size when element of the same type is added" $
---            size (insert (Identity 'b') $ insert (Identity 'a') empty) `shouldBe` 1
---        it "returns 10 when 10 different types are inserted" $
---            size mapOf10 `shouldBe` 10
---
---
---mapOf10 :: TypeRepMap Identity
---mapOf10 = insert (Identity True)
---        $ insert (Identity [True, False])
---        $ insert (Identity $ Just True)
---        $ insert (Identity $ Just ())
---        $ insert (Identity [()])
---        $ insert (Identity ())
---        $ insert (Identity "aaa")
---        $ insert (Identity $ Just 'a')
---        $ insert (Identity 'a')
---        $ insert (Identity (11 :: Int)) empty
+    describe "Size Test" $ do
+        it "is empty" $
+            size empty `shouldBe` 0
+        it "is of size 1 when 1 element inserted" $
+            size (insert (Identity 'a') empty) `shouldBe` 1
+        it "doesn't increase size when element of the same type is added" $
+            size (insert (Identity 'b') $ insert (Identity 'a') empty) `shouldBe` 1
+        it "returns 10 when 10 different types are inserted" $
+            size mapOf10 `shouldBe` 10
+
+
+mapOf10 :: TypeRepMap Identity
+mapOf10 = insert (Identity True)
+        $ insert (Identity [True, False])
+        $ insert (Identity $ Just True)
+        $ insert (Identity $ Just ())
+        $ insert (Identity [()])
+        $ insert (Identity ())
+        $ insert (Identity "aaa")
+        $ insert (Identity $ Just 'a')
+        $ insert (Identity 'a')
+        $ insert (Identity (11 :: Int)) empty
