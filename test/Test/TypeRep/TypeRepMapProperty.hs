1b739d841ee49fb61cfb1f17118b1f9e31237e2a
Chris Penner <christopher.penner@gmail.com>
2020-10-25 04:34:38 -0600

    Add "alter" combinator (#95)
    * Add "alter" combinator
    
    * Add insert, delete, adjust equivalency tests for alter
    
    * Eta reduce
    
    * Compat with old GHC versions
    
    * Fix imports
    
    * Fix constraint typo
    
    * Conditionally add `Eq` instance for old GHC versions in tests

--- test/Test/TypeRep/TypeRepMapProperty.hs
+++ test/Test/TypeRep/TypeRepMapProperty.hs
@@ -20,7 +20,8 @@ import Test.Hspec (Arg, Expectation, Spec, SpecWith, describe, it)
 import Test.Hspec.Hedgehog (hedgehog)
 
 import Data.TypeRepMap.Internal (TypeRepMap (..), WrapTypeable (..), delete, insert, invariantCheck,
-                                 lookup, member, generateOrderMapping, fromSortedList)
+                                 lookup, member, generateOrderMapping, fromSortedList, alter,
+                                 adjust)
 
 import qualified Hedgehog.Gen as Gen
 import qualified Hedgehog.Range as Range
@@ -34,6 +35,10 @@ typeRepMapPropertySpec = describe "TypeRepMap Property tests" $ do
         deleteMemberSpec
         insertInvariantSpec
         deleteInvariantSpec
+        alterInsertSpec
+        alterDeleteSpec
+        alterAdjustSpec
+        alterModifySpec
     describe "Internal helpers" $ do
         generateOrderMappingInvariantSpec
     describe "Instance Laws" $ do
@@ -82,11 +87,42 @@ deleteInvariantSpec = it "invariantCheck (delete k b) == True" $ hedgehog $ do
     WrapTypeable (_ :: IntProxy n) <- forAll genTF
     assert $ invariantCheck (delete @n m)
 
+alterInsertSpec :: Property
+alterInsertSpec = it "insert proxy m === alter (const (Just proxy)) m" $ hedgehog $ do
+    m <- forAll genMap
+    WrapTypeable (proxy :: IntProxy n) <- forAll genTF
+    insert proxy m === alter (const (Just proxy)) m
+
+alterDeleteSpec :: Property
+alterDeleteSpec = it "delete proxy m === alter (const Nothing) m" $ hedgehog $ do
+    WrapTypeable (proxy :: IntProxy n) <- forAll genTF
+    m <- insert proxy <$> forAll genMap
+    delete @n @IntProxy m === alter @n @IntProxy (const Nothing) m
+
+alterAdjustSpec :: Property
+alterAdjustSpec = it "adjust f m == alter (fmap f) m" $ hedgehog $ do
+    m <- forAll genMap
+    WrapTypeable (_ :: IntProxy n) <- forAll genTF
+    let f (IntProxy p n) = IntProxy p (n * 10)
+    adjust @n @IntProxy f m === alter @n @IntProxy (fmap f) m
+
+alterModifySpec :: Property
+alterModifySpec = it "lookup k (alter f) == f (lookup k m)" $ hedgehog $ do
+    m <- forAll genMap
+    WrapTypeable (_ :: IntProxy n) <- forAll genTF
+    randInt <- forAll (Gen.int Range.constantBounded)
+    -- Function with some interesting behaviour, which inserts, seletes and modifies
+    let f Nothing = Just (IntProxy Proxy randInt)
+        f (Just (IntProxy p n))
+          | even n = Nothing
+          | otherwise = Just $ IntProxy p (n * 10)
+    lookup @n @IntProxy (alter @n f m) === f (lookup @n @IntProxy m)
+
 ----------------------------------------------------------------------------
 -- Internal helpers
 ----------------------------------------------------------------------------
 generateOrderMappingInvariantSpec :: Property
-generateOrderMappingInvariantSpec = 
+generateOrderMappingInvariantSpec =
     it "fmap (fromSortedList [1 .. n] !!) (generateOrderMapping n) == [1 .. n]" $ hedgehog $ do
         n <- forAll $ Gen.int (Range.linear 0 100)
         fmap (fromSortedList [1 .. n] !!) (generateOrderMapping n) === [1 .. n]
@@ -95,41 +131,16 @@ generateOrderMappingInvariantSpec =
 -- Semigroup and Monoid laws
 ----------------------------------------------------------------------------
 
-#if __GLASGOW_HASKELL__ < 806
-{- | This newtype is used to compare 'TypeRepMap's using only 'Fingerprint's.
-It's not a good idea to write such 'Eq' instance for 'TypeRepMap' itself because
-it doesn't compare values so it's not true equality. But this should be enough
-for tests.
--}
-newtype FpMap f = FpMap (TypeRepMap f)
-    deriving newtype (Show, Semigroup, Monoid)
-
-instance Eq (FpMap f) where
-    FpMap (TypeRepMap as1 bs1 _ _) == FpMap (TypeRepMap as2 bs2 _ _) =
-        as1 == as2 && bs1 == bs2
-#endif
-
 semigroupAssocSpec :: Property
 semigroupAssocSpec = it "x <> (y <> z) == (x <> y) <> z" $ hedgehog $ do
-#if __GLASGOW_HASKELL__ >= 806
     x <- forAll genMap
     y <- forAll genMap
     z <- forAll genMap
-#else
-    x <- FpMap <$> forAll genMap
-    y <- FpMap <$> forAll genMap
-    z <- FpMap <$> forAll genMap
-#endif
     (x <> (y <> z)) === ((x <> y) <> z)
 
 monoidIdentitySpec :: Property
 monoidIdentitySpec = it "x <> mempty == mempty <> x == x" $ hedgehog $ do
-#if __GLASGOW_HASKELL__ >= 806
     x <- forAll genMap
-#else
-    x <- FpMap <$> forAll genMap
-#endif
-
     x <> mempty === x
     mempty <> x === x
 
@@ -150,3 +161,17 @@ genTF = do
     case someNatVal randNat of
         Just (SomeNat proxyNat) -> pure $ WrapTypeable $ IntProxy proxyNat randInt
         Nothing                 -> error "Invalid test generator"
+
+----------------------------------------------------------------------------
+-- Helpers
+----------------------------------------------------------------------------
+#if __GLASGOW_HASKELL__ < 806
+{- | We add an orphan Eq instance for old GHC versions just to make testing easier.
+It's not a good idea to write such 'Eq' instance for 'TypeRepMap' itself because
+it doesn't compare values so it's not true equality. But this should be enough
+for tests.
+-}
+instance Eq (TypeRepMap f) where
+    TypeRepMap as1 bs1 _ _ == TypeRepMap as2 bs2 _ _ =
+        as1 == as2 && bs1 == bs2
+#endif
